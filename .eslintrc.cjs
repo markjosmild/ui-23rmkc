5e8fba0a5335d5981b7bdf4a0ef4678611b31562
Mark Josmil Degamo <markjosmild@gmail.com>
2023-12-04 21:57:44 +0800

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,227 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:vue/vue3-essential'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      },
+      rules: {}
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'array-bracket-spacing': [
+      'error',
+      'never'
+    ],
+    'no-use-before-define': ['error', {
+      functions: false
+    }],
+    'array-callback-return': 'error',
+    'arrow-parens': [
+      'error',
+      'as-needed'
+    ],
+    'vue/multi-word-component-names': 'off',
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'arrow-spacing': 'error',
+    'block-spacing': 'error',
+    'brace-style': 'error',
+    camelcase: ['error', {
+      properties: 'never'
+    }],
+    'comma-dangle': 'error',
+    'comma-spacing': [
+      'error',
+      {
+        before: false,
+        after: true
+      }
+    ],
+    'default-case': 'error',
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        ignoredNodes: [
+          'TemplateLiteral'
+        ]
+      }
+    ],
+    'no-alert': 'off',
+    'no-await-in-loop': 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'no-else-return': 'error',
+    'no-empty-function': 'error',
+    'no-labels': 'off',
+    'no-loop-func': 'error',
+    'no-multi-spaces': 'error',
+    'no-multiple-empty-lines': [
+      2,
+      {
+        max: 1
+      }
+    ],
+    'no-new': 'off',
+    'no-prototype-builtins': 'off',
+    'no-return-await': 'error',
+    'no-shadow': 'error',
+    'no-useless-catch': 'off',
+    'no-var': 'error',
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'prefer-const': 'error',
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'require-await': 'error',
+    semi: [
+      'error',
+      'never'
+    ],
+    'space-before-function-paren': [
+      'error',
+      'always'
+    ],
+    'template-curly-spacing': 'off',
+    'vue/component-definition-name-casing': [
+      'error',
+      'PascalCase'
+    ],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always'
+      }
+    ],
+    'vue/html-closing-bracket-spacing': ['error', {
+      startTag: 'never',
+      endTag: 'never',
+      selfClosingTag: 'always'
+    }],
+    'vue/html-end-tags': 'error',
+    'vue/html-indent': [
+      'error',
+      2,
+      {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: []
+      }
+    ],
+    'vue/attribute-hyphenation': ['error', 'always'],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'never',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ],
+    'vue/match-component-file-name': [
+      'error',
+      {
+        extensions: [
+          'vue'
+        ],
+        shouldMatchCase: false
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 1
+        },
+        multiline: {
+          max: 1
+        }
+      }
+    ],
+    'vue/no-multi-spaces': ['error', {
+      ignoreProperties: false
+    }],
+    'vue/no-spaces-around-equal-signs-in-attribute': ['error'],
+    'vue/mustache-interpolation-spacing': ['error', 'always'],
+    'vue/multiline-html-element-content-newline': ['error', {
+      ignoreWhenEmpty: true,
+      ignores: ['VueComponent', 'pre', 'textarea'],
+      allowEmptyLines: false
+    }],
+    'vue/first-attribute-linebreak': ['error', {
+      singleline: 'ignore',
+      multiline: 'below'
+    }],
+    'vue/no-unused-properties': [
+      'error',
+      {
+        groups: [
+          'props',
+          'data',
+          'computed',
+          'methods'
+        ]
+      }
+    ],
+    'vue/no-v-html': 'off',
+    'vue/this-in-template': [
+      'error',
+      'never'
+    ],
+    'vue/v-bind-style': [
+      'error',
+      'shorthand'
+    ],
+    'vue/v-on-style': [
+      'error',
+      'shorthand'
+    ],
+    'vue/attributes-order': ['error', {
+      order: [
+        'DEFINITION',
+        'LIST_RENDERING',
+        'CONDITIONALS',
+        'RENDER_MODIFIERS',
+        'GLOBAL',
+        ['UNIQUE', 'SLOT'],
+        'TWO_WAY_BINDING',
+        'OTHER_DIRECTIVES',
+        'OTHER_ATTR',
+        'EVENTS',
+        'CONTENT'
+      ],
+      alphabetical: false
+    }]
+  }
+}
+
